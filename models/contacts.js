debaffdd035f35dd2d4d4594b761a4d4ed650504
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2022-12-20 18:08:28 +0200

    Add 8 function

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,106 @@
-// const fs = require('fs/promises')
+// // const fs = require('fs/promises')
 
-const listContacts = async () => {}
+// const listContacts = async () => {}
 
-const getContactById = async (contactId) => {}
+// const getContactById = async (contactId) => {}
 
-const removeContact = async (contactId) => {}
+// const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+// const addContact = async (body) => {}
 
-const updateContact = async (contactId, body) => {}
+// const updateContact = async (contactId, body) => {}
+
+// module.exports = {
+//   listContacts,
+//   getContactById,
+//   removeContact,
+//   addContact,
+//   updateContact,
+// }
+
+
+
+const fs = require('fs/promises');
+const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+
+const contactsPath = path.join(__dirname, "contacts.json");
+
+const listContacts = async () => {
+  try {
+    const contactsList = await fs.readFile(contactsPath);
+    return JSON.parse(contactsList);
+  } 
+  catch (error) {
+    console.error(error)
+}
+}
+
+const getContactById = async (contactId) => {
+  try {
+    const contactsList = await listContacts();
+    const contactById = contactsList.find(contact => contact.id === contactId);
+    return contactById || null;
+  } 
+  catch (error) {
+    console.error(error)
+} 
+}
+
+const removeContact = async (contactId) => {
+  try {
+    const contactsList = await listContacts();
+    const indexById = contactsList.findIndex(contact => contact.id === contactId);
+
+    if (indexById  === -1) {
+        return null;
+    }
+    const [deleteContact ]= contactsList.splice(indexById, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contactsList, null, 2));
+    return deleteContact;
+  } 
+  catch (error) {
+    console.error(error)
+}
+}
+
+const addContact = async (body) => {
+  try {
+    const contactsList = await listContacts();
+    const newContact = {
+        id: uuidv4(),
+        name: body.name,
+        email: body.email,
+        phone: body.phone,
+    }
+    contactsList.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contactsList, null, 2));
+    return newContact;
+  } 
+  catch (error) {
+    console.error(error)
+}
+}
+
+const updateContact = async (contactId, body) => {
+  try {
+    const contactsList = await listContacts();
+    const contactById = contactsList.find(contact => contact.id === contactId);
+    if (contactById !== null) {
+      const updateContactById = {
+        name: body.name,
+        email: body.email,
+        phone: body.phone
+      }
+      contactsList.push(updateContactById);
+      await fs.writeFile(contactsPath, JSON.stringify(contactsList, null, 2));
+      return updateContactById || null;
+    }
+  } 
+  catch (error) {
+    console.log(error)
+  }
+}
 
 module.exports = {
   listContacts,
@@ -17,3 +109,22 @@ module.exports = {
   addContact,
   updateContact,
 }
+
+// ==========================================================================
+
+// async function addContact({name, email, phone}) {
+// try {
+//     const contactsList = await listContacts();
+//     const newContact = {
+//         id: uuidv4(),
+//         name,
+//         email,
+//         phone,
+//     }
+//     contactsList.push(newContact);
+//     await fs.writeFile(contactsPath, JSON.stringify(contactsList, null, 2));
+//     return newContact;
+// } catch (error) {
+//     console.error(error)
+// }
+// };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,13 +1,48 @@
 const express = require('express')
+const contacts = require('../../models/contacts')
+const { HttpError } = require('../../helpers')
 
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+try {
+  const allContatcts = await contacts.listContacts();
+  res.json(allContatcts)
+  } 
+  catch (error) {
+    next(error)
+  // res.status(500).json({
+  //   message: 'Server error'
+  // })
+}
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const id = req.params.contactId;
+    console.log(id);
+    const contactById = await contacts.getContactById(id);
+
+    if (!contactById) {
+      throw HttpError(404, 'Not found')
+      // const error = new Error('Not found');
+      // error.status = 404;
+      // throw error;
+
+      // return res.status(404).json({
+      //   message: 'Not found'
+      // })
+    };
+
+    res.json(contactById)
+    } 
+    catch (error) {
+      next(error)
+      // const {status = 500, message = 'Server error'} = error;
+      // res.status(status).json({
+      //   message,
+      // })
+  }
 })
 
 router.post('/', async (req, res, next) => {
